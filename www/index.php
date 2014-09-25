260bc7b6869a6bf1d2d98a73aa915868f44f4fc2
Russley Shaw <russleyshaw@gmail.com>
2014-09-25 13:34:33 -0500

    Added navbar
    Moved bootstrap folder

--- www/index.php
+++ www/index.php
@@ -1,7 +1,29 @@
-<html>
-<title>GFMS</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<body>
-    <h1>Group Finance Management System</h1>
-</body>
+  <title>Home | GFMS</title>
+
+  <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/bootstrap/css/custom.css" rel="stylesheet">
+</head>
+
+<body> <!-- BODY START -->
+
+  
+  <!-- NAVBAR INCLUDE --> <?php 
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/navbar/default.php";
+      include_once($path);
+  ?>
+
+  <h1>Hello, world!</h1>
+
+  <!-- Scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="/bootstrap/js/bootstrap.min.js"></script>
+</body> <!-- BODY END -->
 </html>
