92f9b409805109cea551743e4aab374ed56f4139
Russley Shaw <russleyshaw@gmail.com>
2014-09-26 10:37:09 -0500

    Added secret_conn.php template.

--- www/conn.php
+++ www/conn.php
@@ -3,7 +3,7 @@
   $path .= "/secret_conn.php";
   include_once($path);
 
-  $mysqli = new mysqli($DB_URL, $DB_USER, $DB_PASS, $DB_NAME); 
+  $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME); 
 
   if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
